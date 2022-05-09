59a5703eb1c0024977af128f0ca133a9966eabd7
truongthanhhai283 <truongthanhhai283@gmail.com>
2022-05-09 17:11:27 +0700

    Task#21: Normalize product/product images

--- ecommerce-next/framework/shopify/product/get-all-product.ts
+++ ecommerce-next/framework/shopify/product/get-all-product.ts
@@ -1,6 +1,7 @@
 import fetchApi from "@framework/utils/fetch-api"
 import getAllProductsQuery from "@framework/utils/queries/get-all-products"
 import { ProductConnection } from "@framework/schema"
+import { normalizeProduct } from '../utils/normalize'
 
 type ReturnType = {
     products: ProductConnection
@@ -9,12 +10,11 @@ type ReturnType = {
 const getAllProducts = async (): Promise<any> => {
     const { data } = await fetchApi<ReturnType>({ query: getAllProductsQuery })
 
-    const products = data.data.products.edges.map(({ node: product }) => {
-        return product
+    const products = data.products.edges.map(({ node: product }) => {
+        return normalizeProduct(product)
     }) ?? []
 
     return products
-
 }
 
 export default getAllProducts;
