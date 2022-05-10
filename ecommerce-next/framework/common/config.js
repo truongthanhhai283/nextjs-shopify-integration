9430045f15fe8b39e2060568aa446a89469ae2a2
truongthanhhai283 <truongthanhhai283@gmail.com>
2022-05-10 17:02:50 +0700

    Task#24: Ts config specify setup, format JSON config file

--- ecommerce-next/framework/common/config.js
+++ ecommerce-next/framework/common/config.js
@@ -1,8 +1,10 @@
 const path = require("path");
 const merge = require("deepmerge");
+const fs = require("fs");
+const prettier = require("prettier");
 
 function withFrameworkConfig(defaultConfig = {}) {
-  const framework = "shopify";
+  const framework = defaultConfig?.framework.name;
 
   const frameworkNextConfig = require(path.join(
     "../",
@@ -11,6 +13,17 @@ function withFrameworkConfig(defaultConfig = {}) {
   ));
 
   const config = merge(defaultConfig, frameworkNextConfig);
+
+  const tsPath = path.join(process.cwd(), "tsconfig.json");
+  const tsConfig = require(tsPath);
+
+  tsConfig.compilerOptions.paths["@framework"] = [`framework/${framework}`];
+  tsConfig.compilerOptions.paths["@framework/*"] = [`framework/${framework}/*`];
+
+  fs.writeFileSync(
+    tsPath,
+    prettier.format(JSON.stringify(tsConfig), { parser: "json" })
+  );
 
   return config;
 }
