6e9ce66bbe5cf9a0ef7a3d7c174112357a059dae
truongthanhhai283 <truongthanhhai283@gmail.com>
2022-05-18 10:59:19 +0700

    Task#36: Flex hero, marquee

--- ecommerce-next/pages/index.tsx
+++ ecommerce-next/pages/index.tsx
@@ -5,6 +5,7 @@ import { Layout } from "@components/common";
 import { ProductCard } from "@components/product";
 import { Grid } from "@components/ui";
 import { Hero } from "@components/ui";
+import { Marquee } from "@components/ui";
 
 // 2. get all product
 import getAllProducts from "@framework/product/get-all-product";
@@ -36,6 +37,11 @@ const Home = ({
         headline="Cookies, ice cream and muffin"
         description="Tootsie roll wafer pie gingerbread bonbon apple pie ice cream. Marzipan ice cream chocolate caramels chocolate chocolate bar tootsie roll. Wafer jelly toffee jelly-o cake wafer cotton candy pastry oat cake. Danish dragée toffee oat cake biscuit chocolate brownie. Jujubes sweet icing candy sweet roll gummies topping. Marshmallow donut pastry cookie pudding. Tootsie roll tootsie roll oat cake liquorice marzipan liquorice marzipan. Gummies muffin wafer candy canes cupcake cupcake shortbread tootsie roll. Dessert macaroon pastry chupa chups topping pastry icing liquorice. Macaroon gummies sugar plum cheesecake liquorice. Bear claw pudding gummi bears candy sweet roll gummi bears. Pastry bonbon jelly beans tart topping pastry."
       />
+      <Marquee>
+        {productsList.slice(0, 3).map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </Marquee>
     </>
   );
 };
