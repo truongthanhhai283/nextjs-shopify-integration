6d1527f7fd2e995abaa7eaf7701d14c01417e407
truongthanhhai283 <truongthanhhai283@gmail.com>
2022-05-18 16:21:48 +0700

    Task#37: Slim/variant product, variant maquee, ticker, layout b

--- ecommerce-next/components/product/ProductCard/ProductCard.tsx
+++ ecommerce-next/components/product/ProductCard/ProductCard.tsx
@@ -6,37 +6,61 @@ import Image from "next/image";
 import style from "./ProductCard.module.css";
 interface Props {
   product: Product;
+  variant?: "simple" | "slim";
 }
 
 const placeholderImage = "/product-image-placeholder.svg";
 
-const ProductCard: FC<Props> = ({ product }) => {
+const ProductCard: FC<Props> = ({ product, variant = "simple" }) => {
   return (
     <>
       <Link href={`/products/${product.slug}`}>
         <a className={style.root}>
-          <div className={style.productBg}></div>
-          <div className={style.productTag}>
-            <h3 className={style.productTitle}>
-              <span>{product.name}</span>
-            </h3>
-            <span className={style.productPrice}>
-              {product.price.value} {product.price.currencyCode}
-            </span>
-          </div>
-          <div>
-            {product.images && (
-              <Image
-                className={style.productImage}
-                alt={product.name ?? "Product image"}
-                src={product.images[0].url ?? placeholderImage}
-                height={540}
-                width={540}
-                quality="85"
-                layout="responsive"
-              />
-            )}
-          </div>
+          {variant === "slim" ? (
+            <>
+              <div className="inset-0 flex items-center justify-center absolute z-20">
+                <span className="bg-black text-white p-3 font-bold text-xl">
+                  {product.name}
+                </span>
+              </div>
+              {product.images && (
+                <Image
+                  className={style.productImage}
+                  alt={product.name ?? "Product image"}
+                  src={product.images[0].url ?? placeholderImage}
+                  height={320}
+                  width={320}
+                  quality="85"
+                  layout="fixed"
+                />
+              )}
+            </>
+          ) : (
+            <>
+              <div className={style.productBg}></div>
+              <div className={style.productTag}>
+                <h3 className={style.productTitle}>
+                  <span>{product.name}</span>
+                </h3>
+                <span className={style.productPrice}>
+                  {product.price.value} {product.price.currencyCode}
+                </span>
+              </div>
+              <div>
+                {product.images && (
+                  <Image
+                    className={style.productImage}
+                    alt={product.name ?? "Product image"}
+                    src={product.images[0].url ?? placeholderImage}
+                    height={540}
+                    width={540}
+                    quality="85"
+                    layout="responsive"
+                  />
+                )}
+              </div>
+            </>
+          )}
         </a>
       </Link>
     </>
--- ecommerce-next/components/ui/Marquee/Marquee.tsx
+++ ecommerce-next/components/ui/Marquee/Marquee.tsx
@@ -1,13 +1,26 @@
 import { FC, ReactNode } from "react";
+import Ticker from "react-ticker";
+import cn from "classnames";
 
 import style from "./Marquee.module.css";
 
 interface Props {
   children: ReactNode[];
+  variant: "primary" | "secondary";
 }
 
-const Marquee: FC<Props> = ({ children }) => {
-  return <div className={style.root}>{children}</div>;
+const Marquee: FC<Props> = ({ children, variant }) => {
+  const rootClassName = cn(style.root, {
+    [style.secondary]: variant === "secondary",
+  });
+
+  return (
+    <div className={rootClassName}>
+      <Ticker offset={80}>
+        {() => <div className={style.container}>{children}</div>}
+      </Ticker>
+    </div>
+  );
 };
 
 export default Marquee;
