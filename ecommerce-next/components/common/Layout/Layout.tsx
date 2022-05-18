85826e559382f9ba65de9d985092c1361fac53be
truongthanhhai283 <truongthanhhai283@gmail.com>
2022-05-18 17:26:17 +0700

    Task#38: Navbar, footer

--- ecommerce-next/components/common/Layout/Layout.tsx
+++ ecommerce-next/components/common/Layout/Layout.tsx
@@ -1,11 +1,15 @@
 import { FC } from "react";
 
+import { Footer } from "@components/common";
+import { Navbar } from "@components/common";
 import style from "./Layout.module.css";
 
 const Layout: FC = ({ children }) => {
   return (
     <div className={style.root}>
+      <Navbar />
       <main className="fit">{children}</main>
+      <Footer />
     </div>
   );
 };
