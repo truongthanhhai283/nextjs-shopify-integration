24946308548caef7b2d7b5289ca49b6af2679eda
truongthanhhai283 <truongthanhhai283@gmail.com>
2022-05-20 14:27:26 +0700

    Task#39: Ui provider sidebar - Build sidebars, wishlist

--- ecommerce-next/components/common/Layout/Layout.tsx
+++ ecommerce-next/components/common/Layout/Layout.tsx
@@ -1,13 +1,18 @@
 import { FC } from "react";
 
-import { Footer } from "@components/common";
-import { Navbar } from "@components/common";
+import { Navbar, Footer } from "@components/common";
+import { Sidebar } from "@components/ui";
+import { CartSidebar } from "@components/cart";
+
 import style from "./Layout.module.css";
 
 const Layout: FC = ({ children }) => {
   return (
     <div className={style.root}>
       <Navbar />
+      <Sidebar>
+        <CartSidebar />
+      </Sidebar>
       <main className="fit">{children}</main>
       <Footer />
     </div>
