be508cf8cd429f494d8f7a77c10606f61dd4c929
truongthanhhai283 <truongthanhhai283@gmail.com>
2022-05-20 15:51:05 +0700

    Task#40: UI Provider, state in UI Provider, provider interface

---
+++ ecommerce-next/components/ui/context.tsx
@@ -0,0 +1,42 @@
+import { createContext, FC, useContext, useState } from "react";
+
+export interface StateModifiers {
+  openSidebar: () => void;
+  closeSidebar: () => void;
+}
+
+export interface StateValues {
+  isSidebarOpen: boolean;
+}
+
+const stateModifiers = {
+  openSidebar: () => {},
+  closeSidebar: () => {},
+};
+
+const initialState = { isSidebarOpen: false };
+
+type State = StateValues & StateModifiers;
+
+const UIContext = createContext<State>({
+  ...stateModifiers,
+  ...initialState,
+});
+
+export const UIProvider: FC = ({ children }) => {
+  const openSidebar = () => alert("123");
+  const closeSidebar = () => alert("456");
+
+  const value = {
+    openSidebar,
+    closeSidebar,
+    isSidebarOpen: true,
+  };
+
+  return <UIContext.Provider value={value}>{children}</UIContext.Provider>;
+};
+
+export const useUI = () => {
+  const context = useContext(UIContext);
+  return context;
+};
